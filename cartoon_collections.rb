b09f7eb09c25e10054c41453172559655921cb36
Zakari Badiru <zbadiru@live.co.uk>
2020-04-04 21:54:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 def long_planeteer_calls(planeteer_calls)
 planeteer_calls.any? do |call|
   call.length > 4 
-  # or planeteer_calls.any { |call| call.lenght
+  # or planeteer_calls.any { |call| call.lenght > 4 }
 end
 end
 

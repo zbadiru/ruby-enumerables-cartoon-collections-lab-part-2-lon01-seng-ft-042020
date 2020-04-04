9c15b083c79688c0a1572983324cb7e57f7bbc5f
Zakari Badiru <zbadiru@live.co.uk>
2020-04-04 21:39:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |
+  planeteer_calls.map do |call|
 end
 
 def long_planeteer_calls(planeteer_calls)

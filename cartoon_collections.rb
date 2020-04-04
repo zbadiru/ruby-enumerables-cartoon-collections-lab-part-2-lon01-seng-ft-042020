acdd14693105acd5baf4ceb8f5a671ec4615158e
Zakari Badiru <zbadiru@live.co.uk>
2020-04-04 21:19:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
-  # Return a new array of the results
+
   
 end
 

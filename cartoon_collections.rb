2868296c1108350fbe06ad5ab8042d60b133f571
Zakari Badiru <zbadiru@live.co.uk>
2020-04-04 21:52:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,6 @@ end
 end
 
 def find_valid_calls(planeteer_calls)
-planeteer_calls.find {|call| find_valid_calls.include?(call)}
+  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+planeteer_calls.find {|call| valid_calls.include?(call)}
 end
